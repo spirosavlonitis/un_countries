a222f0f6178e256adf870d56ca8ae264e2c98413
Spiros Avlonitis <spirosa84@hotmail.com>
2017-02-16 06:33:07 +0200

    added countries

--- lib/un_countries.rb
+++ lib/un_countries.rb
@@ -4,7 +4,13 @@ module UnCountries
    def self.countries_list
     
     countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Antigua and Barbuda",
-    	"Argentina","Armenia","Australia","Austria"]
+    	"Argentina","Armenia","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh",
+    	"Barbados","Belarus","Belgium","Belize","Benin","Bhutan","Bolivia","Bosnia and Herzegovina",
+    	"Botswana","Brazil","Brunei Darussalam","Bulgaria","Burkina Faso","Burundi","Cabo Verde","Cambodia",
+    	"Cameroon","Canada","Central African Republic","Chad","Chile","China","Colombia","Comoros","Congo",
+    	"Costa Rica","Côte d'Ivoire","Croatia","Cuba","Cyprus","Czech Republic","Democratic Republic of the Congo",
+    	"Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea",
+    	"Estonia","Ethiopia","Fiji","Finland","France","Gabon","Gambia","Georgia","Germany","Ghana","Greece"]
     return countries
   end
 
